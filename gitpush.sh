a690c985d09043596d2c8b1ad4b6fec9deebc06d
= <=>
2015-04-01 16:04:56 -0300

    Enhanced scripts

--- gitpush.sh
+++ gitpush.sh
@@ -4,14 +4,15 @@
 echo "Pushing your current version to the repository..."
 
 # add any new files that may have been created
-git add . > /dev/null
+git add -q . >/dev/null
 
 # put all these changes into a commit
+# -q: shut up
 # -a: all changes
 # -m "message": add commit message
-git commit -am "$1" > /dev/null
+git commit -qam "$1" >/dev/null
 
 # push the commits to the repository
-git push origin master > /dev/null
+git push -q origin master >/dev/null
 
 echo "Done."
